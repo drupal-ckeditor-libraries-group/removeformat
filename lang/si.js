09f523b4f322e8e38c04935de5f2c8702bdffdc9
Wiktor Walc <w.walc@cksource.com>
2013-06-10 13:22:00 +0200

    Updated translations, added Sinhala language.

--- lang/si.js
+++ lang/si.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'removeformat', 'gl', {
-	toolbar: 'Retirar o formato'
+CKEDITOR.plugins.setLang( 'removeformat', 'si', {
+	toolbar: 'සැකසීම වෙනස් කරන්න'
 });
