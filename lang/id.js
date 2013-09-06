cbdeea261754732066caff460113079482201839
Wiktor Walc <w.walc@cksource.com>
2013-09-06 16:21:36 +0200

    Updated translations

--- lang/id.js
+++ lang/id.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'removeformat', 'ja', {
-	toolbar: '書式を解除'
+CKEDITOR.plugins.setLang( 'removeformat', 'id', {
+	toolbar: 'Hapus Format'
 });
